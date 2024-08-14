1c860ffa13a6c8cac7d0031978bb932ece3c5dc8
Ahmed Farooq <160997141+ahmed-workstreamautomation@users.noreply.github.com>
2024-08-14 20:57:06 +0500

    Search Bar UI/UX functionality done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { IoSearch, IoClose, IoInformationCircleOutline, IoHomeOutline } from 're
 import { FaBars } from "react-icons/fa6";
 import { FaLaptopCode } from "react-icons/fa";
 import SignInButton from './SignInButton'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 const sideNavLinks = [
@@ -26,16 +26,20 @@ const sideNavLinks = [
 ]
 
 const Navbar = () => {
+
     const [showSidebar, setShowSidebar] = useState(false)
+    const [showSearchBar, setShowSearchBar] = useState(false)
 
   return (
     <div className='flex items-center justify-between bg-[--header-bg-color] dark:bg-[--header-bg-color-dark] border-b border-gray-300 dark:border-gray-500 dark:border-opacity-50 py-4 px-3 sm:px-6 md:px-10 lg:px-12 2xl:px-14'>
         <Link to="/" className='font-bold text-base md:text-xl text-black dark:text-white'><span className='bg-gradient-to-r from-[#b92b27] to-[#1565C0] px-3 py-2 rounded-tl-3xl rounded-br-3xl text-white'>A.F</span>Blogspot</Link>
-        <form className='relative lg:flex items-center min-w-52 hidden'>
+        <button className='border border-gray-500 rounded-full px-4 py-2 lg:hidden' onClick={() => setShowSearchBar(!showSearchBar)}>
+            {showSearchBar ? <IoClose /> : <IoSearch />}
+        </button>
+        <form className={`${showSearchBar && window.innerWidth < 1024 ? 'absolute flex items-center left-0 right-0 w-fit mx-auto top-[75px]' : 'relative lg:flex items-center min-w-52 hidden'}`}>
             <input type='text' placeholder='Search...' className='w-full bg-transparent outline-none border border-gray-500 rounded-lg py-2 px-2 focus:border-blue-600' />
             <span className='absolute right-2'><IoSearch /></span>
         </form>
-        <button className='border border-gray-500 rounded-full px-4 py-2 lg:hidden'><IoSearch /></button>
         <ul className={`fixed h-screen top-0 ${showSidebar ? 'right-0' : 'right-[-100%]'} bg-[#E2E1DF] dark:bg-[var(--sidenav-bg-color-dark)] w-[250px] 
         sm:bg-transparent sm:dark:bg-transparent sm:h-auto sm:w-fit sm:relative sm:right-0 flex flex-col sm:flex-row sm:items-center sm:justify-center sm:gap-8 sm:text-sm md:text-base
         transition-all duration-500 ease-in-out py-20 sm:py-0 px-2 sm:px-0`}>
